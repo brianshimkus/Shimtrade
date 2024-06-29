9684e41b21ad38be071112be66d8e4a8f779e76f
Brian Shimkus <brianshimkus1@gmail.com>
2024-06-28 23:46:35 -0500

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import connectDB from './db/connection.js'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5050
+
+connectDB()
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// start the Express server
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`)
+	console.log(`Server listening on port ${PORT}`.yellow)
 })
